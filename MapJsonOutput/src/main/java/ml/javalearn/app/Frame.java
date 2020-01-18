1766461dd312206607e70b1872c7d6d7f319055f
igorok-follow <podmihka@gmail.com>
2020-01-18 21:24:29 +0800

    Added the ability to build paths between nodes.

--- MapJsonOutput/src/main/java/ml/javalearn/app/Frame.java
+++ MapJsonOutput/src/main/java/ml/javalearn/app/Frame.java
@@ -3,17 +3,29 @@ package ml.javalearn.app;
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 class Frame extends JFrame {
 
     private ArrayList<Node> nodes;
     private ArrayList<Edge> edges;
     private Parser parser;
+    private HashMap<Integer, Node> getNameUseId;
 
     Frame(Parser parser) {
         this.parser = parser;
+        fillMap();
         init();
         setPanel();
+    }
+
+    private void fillMap() {
+        getNameUseId = new HashMap<>();
+        nodes = parser.getNodesList();
+        edges = parser.getEdgesList();
+        for (Node node : nodes) {
+            getNameUseId.put(node.getId(), node);
+        }
     }
 
     private void setPanel() {
@@ -21,11 +33,28 @@ class Frame extends JFrame {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                nodes = parser.getNodesList();
-                edges = parser.getEdgesList();
-                for (Node node : nodes) {
-                    g.drawOval(node.getX() * 100, node.getY() * 100, 25, 25);
+
+                int from_id, to_id;
+                for (Edge edge : edges) {
+                    try {
+                        from_id = edge.getFrom_id();
+                        to_id = edge.getTo_id();
+                        Node fromNode = getNameUseId.get(from_id);
+                        Node toNode = getNameUseId.get(to_id);
+                        g.drawLine(fromNode.getX() * 100 + 42, fromNode.getY() * 100 + 42,
+                                toNode.getX() * 100 + 42, toNode.getY() * 100 + 42);
+                    } catch (Exception ex) {
+                        System.out.println("Not found some edges");
+                    }
                 }
+
+                for (Node node : nodes) {
+                    g.setColor(new Color(0x3E3E3E));
+                    g.fillOval(node.getX() * 100 + 30, node.getY() * 100 + 30, 25, 25);
+                    g.setColor(new Color(0x007C7B));
+                    g.drawString(node.getName(), node.getX() * 100 + 20, node.getY() * 100 + 20);
+                }
+
             }
         };
 
@@ -35,7 +64,7 @@ class Frame extends JFrame {
     private void createFrame() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("JSON Map");
-        setSize(1000, 825);
+        setSize(1100, 825);
         setVisible(true);
     }
 
